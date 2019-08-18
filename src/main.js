9419a6d42a25bc02bc9d05f314ea9bfc84bcf40d
fabio <fabiolopes.dev@gmail.com>
2019-08-18 15:21:41 -0300

    Loading State

--- src/main.js
+++ src/main.js
@@ -13,18 +13,38 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
+    setLoading(loading = true){
+        if(loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id', 'loading');
+            this.formEl.appendChild(loadingEl);
+        }else{
+            document.getElementById('loading').remove();
+        }
+    }
+
     async addRepository(event){
         event.preventDefault();
 
         const repoInput = this.inputEl.value;
         if(repoInput.length === 0)
             return;
+
+
+        this.setLoading();
+        try{
         
         const response = await api.get(`/repos/${repoInput}`);
         const {name, description, html_url, owner:{avatar_url}} = response.data;
         this.repositories.push({name,description,avatar_url,html_url});
         this.render();
         this.inputEl.value='';
+        }catch(err){
+            alert('O repositório não existe!');
+        }
+
+        this.setLoading(false);
     }
 
     render(){
